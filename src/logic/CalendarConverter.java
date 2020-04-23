e37a50412e8e6d352928f1ec4e9e4783682d258a
Kamil Zalwert <kamilz89@gmail.com>
2020-04-23 17:02:44 +0200

    zaj 6 zad 4; poprawka dla switch i stałych (final static)

--- src/logic/CalendarConverter.java
+++ src/logic/CalendarConverter.java
@@ -1,26 +1,26 @@
 package logic;
 
 public class CalendarConverter {
-    public static int   MONDAY       = 1,
-                        TUESDAY      = 2,
-                        WEDNESDAY    = 3,
-                        THURSDAY     = 4,
-                        FRIDAY       = 5,
-                        SATURDAY     = 6,
-                        SUNDAY       = 7;
+    public final static int     MONDAY       = 1,
+                                TUESDAY      = 2,
+                                WEDNESDAY    = 3,
+                                THURSDAY     = 4,
+                                FRIDAY       = 5,
+                                SATURDAY     = 6,
+                                SUNDAY       = 7;
 
     public static int[] DAYSOFWEEK = {MONDAY,TUESDAY,WEDNESDAY,THURSDAY,FRIDAY,SATURDAY,SUNDAY};
 
     public static String convertDayToString(int dayNumber){
         return switch(dayNumber){
-                case 1 -> "Poniedziałek";
-                case 2 -> "Wtorek";
-                case 3 -> "Środa";
-                case 4 -> "Czwartek";
-                case 5 -> "Piątek";
-                case 6 -> "Sobota";
-                case 7 -> "Niedziela";
-                default -> null;
+                case MONDAY      -> "Poniedziałek";
+                case TUESDAY     -> "Wtorek";
+                case WEDNESDAY   -> "Środa";
+                case THURSDAY    -> "Czwartek";
+                case FRIDAY      -> "Piątek";
+                case SATURDAY    -> "Sobota";
+                case SUNDAY      -> "Niedziela";
+                default          -> null;
         };
     }
 }
